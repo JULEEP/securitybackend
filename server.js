8812bb1e90d43920d01c95a71ca21444918f7771
MAHALAKSHMI837 <incognitoperson674@gmail.com>
2025-04-25 18:04:57 +0530

    create invoice and reports

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import userRoutes from "./routes/user.routes.js";
 import projectRoutes from "./routes/project.routes.js";
-
+import invoiceRouters from "./routes/invoice.routes.js";
 
 dotenv.config();
 
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Routes
 app.use("/api/freelancers", userRoutes); //fixed spelling error freeliancers to freelancers
 app.use("/api/projects", projectRoutes);
+app.use("/api/invoices",invoiceRouters)
 
 
 
